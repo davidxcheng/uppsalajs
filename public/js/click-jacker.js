ada6076845b42c34ed0e2b31f8beccbc1d9b6a32
David Cheng <davidxcheng@gmail.com>
2015-01-15 17:09:25 +0100

    Added event content

--- public/js/click-jacker.js
+++ public/js/click-jacker.js
@@ -1,11 +1,15 @@
 (function() {
-	document.addEventListener("click", function(e) {
-		if (e.target.dataset["contentLink"]) {
-			e.preventDefault();
+    document.addEventListener("click", function(e) {
+        if (e.target.dataset["contentLink"]) {
+            e.preventDefault();
 
-			var contentId = e.target.dataset["contentLink"];
+            var contentId = e.target.dataset["contentLink"];
             content.innerHTML = $("#" + contentId).text();
             pag.innerHTML = $("#" + contentId.substr(0, 3) + "-pag").text();
-		}
-	});
+        }      
+    });
+
+    document.addEventListener("add/artist", function(e) {
+        content.innerHTML = "<img src='img/swift.jpeg'><div>" + e.detail.name + "</div>";
+    });
 })();
